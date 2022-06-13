b2d10120b3f396263258baa2372e8902243e5e6f
MCHP-MAIN\I62419 <sindhu.holla@microchip.com>
2022-06-13 12:44:17 +0530

    Changed the MCC configuration

--- mclv-48v-300w-dspic33ck256mp508-an1017/bldc.X/mcc_generated_files/system.c
+++ mclv-48v-300w-dspic33ck256mp508-an1017/bldc.X/mcc_generated_files/system.c
@@ -46,20 +46,20 @@
 #include "clock.h"
 #include "system.h"
 #include "system_types.h"
-#include "adc1.h"
+#include "tmr1.h"
 #include "cmp1.h"
-#include "uart1.h"
-#include "sccp3_tmr.h"
+#include "pwm.h"
 #include "interrupt_manager.h"
 #include "traps.h"
-#include "tmr1.h"
-#include "pwm.h"
+#include "uart1.h"
+#include "sccp3_tmr.h"
+#include "adc1.h"
 
 void SYSTEM_Initialize(void)
 {
     PIN_MANAGER_Initialize();
-    INTERRUPT_Initialize();
     CLOCK_Initialize();
+    INTERRUPT_Initialize();
     SCCP3_TMR_Initialize();
     CMP1_Initialize();
     UART1_Initialize();
